5c1333061c818e3459b8718d36375420424f4fe0
Dong Woo Lee <don04lee@gmail.com>
2024-01-05 15:56:54 -0800

    Add footer and UI for the weather page.

--- src/index.js
+++ src/index.js
@@ -15,29 +15,23 @@ button.addEventListener("click", function(event) {
 
 async function getWeather(text) {
   let textInput = text.value;
-  // let city = document.createElement('div');
-  // let region = document.createElement('div');
-  // let temp_c = document.createElement('div');
-  // let temp_f = document.createElement('div');
-  // let feelslike_c = document.createElement('div');
-  // let feelslike_f = document.createElement('div');
-  // let condition = document.createElement('div');
+  let place = document.getElementById('place');
+  let temp = document.getElementById('temp');
+  let feelsliketemp = document.getElementById('feelsliketemp');
+  let condition = document.getElementById('condition');
 
   if(textInput !== null) {
     try {
       let response = await fetch('http://api.weatherapi.com/v1/current.json?key=e9b6aed4d9a549eebe9235738240301&q=' + textInput.toString());
       let weatherData = await response.json();
+      place.textContent = weatherData.location.name + ",\n" + weatherData.location.region;
+      temp.textContent = weatherData.current.temp_c + "°C / " + weatherData.current.temp_f + "°F";
+      feelsliketemp.textContent = "Feels Like: " + weatherData.current.feelslike_c + "°C / " + weatherData.current.feelslike_f + "°F"
+      condition.textContent = weatherData.current.condition.text;
       console.log(weatherData);
-      // city = weatherData.location.name;
-      // region = weatherData.location.region;
-      // temp_c = weatherData.current.temp_c;
-      // temp_f = weatherData.current.temp_f;
-      // feelslike_c = weatherData.current.feelslike_c;
-      // feelslike_f = weatherData.current.feelslike_f;
-  //    condition = weatherData.current.condition.text;
     }
     catch(err) {
-      console.log("No available city.");
+      temp.textContent = "No available city.";
     }
   }
 }
